16c3e4024835cf41c811be2ad7f427b5e3323259
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-09-28 22:36:49 +0600

    font and daisy UI theme added

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#67e8f9",
+        
+"secondary": "#d8b4fe",
+        
+"accent": "#fbbf24",
+        
+"neutral": "#4b5563",
+        
+"base-100": "#f3f4f6",
+        
+"info": "#3ABFF8",
+        
+"success": "#36D399",
+        
+"warning": "#FBBD23",
+        
+"error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
